fcc87fd3593726fd7a620e371bfa15a302491fc3
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2021-08-18 22:46:51 +0530

    fix tail

--- pici/app.py
+++ pici/app.py
@@ -95,7 +95,9 @@ class App:
         print("PiCI: Stopped app")
 
     def tail(self):
-        tailproc = subprocess.Popen(['/usr/bin/tail', '-f', '.pici/outputs/' + self.name + '.out.log'], stdout=sys.stdout, stderr=sys.stderr, stdin=sys.stdin)
+        tailproc = subprocess.Popen(['/usr/bin/tail', '-f', '.pici/outputs/' + self.name + '.out.log'], stdout=subprocess.PIPE, stdin=sys.stdin)
+        for line in tailproc.stdout:
+            print(line)
         tailproc.wait()
 
     def setup_outputs(self):
