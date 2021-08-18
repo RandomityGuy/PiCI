c4b74d40c6a099a3eeeca8d5990ad9da4ea6ad7b
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2021-08-18 21:55:15 +0530

    output fixes and persistency things

--- main.py
+++ main.py
@@ -1,31 +1,57 @@
-from os import mkdir
+
 import pici.app as app
 import os
+import shutil
 
-print("PICI: 1.0.0")
+print("PiCI: 1.0.0")
 print("Commands:")
+print("help: show help")
 print("load <app.json>: build app.json")
 print("apps: list available apps")
 print("list: list running apps")
 print("start <app_name>: start app with name <app_name>")
 print("stop <app_name>: stop app with name <app_name>")
-print("build <app_name>: build app with name <app_name>")
+print("build <app_name> [-s]: build app with name <app_name>, -s: silent")
 print("tail <app_name>: tail log of app with name <app_name>")
-print("exit: exit pici")
+print("output <app_name>: output log of app with name <app_name>")
+print("error <app_name>: error log of app with name <app_name>")
+print("exit: exit PiCI")
 
 os.makedirs('.pici/apps', exist_ok=True)
+os.makedirs('.pici/configs', exist_ok=True)
 
 apps: list[app.App] = []
+
+for config in os.listdir('.pici/configs'):
+    if not config.endswith('.json'):
+        continue
+    thisapp = app.App()
+    if thisapp.load(os.path.join('.pici/configs', config)):
+        apps.append(thisapp)
 
 while True:
     cmd = input(">>> ")
     cmd_args = cmd.split(" ")
+    if cmd_args[0] == "help":
+        print("Commands:")
+        print("help: show help")
+        print("load <app.json>: build app.json")
+        print("apps: list available apps")
+        print("list: list running apps")
+        print("start <app_name>: start app with name <app_name>")
+        print("stop <app_name>: stop app with name <app_name>")
+        print("build <app_name> [-s]: build app with name <app_name>, -s: silent")
+        print("tail <app_name>: tail log of app with name <app_name>")
+        print("output <app_name>: output log of app with name <app_name>")
+        print("error <app_name>: error log of app with name <app_name>")
+        print("exit: exit PiCI")
     if cmd_args[0] == "load":
         if len(cmd_args) == 2:
             thisapp = app.App()
             if thisapp.load(cmd_args[1]):
                 apps.append(thisapp)
                 print("Loaded " + cmd_args[1])
+                shutil.copy(cmd_args[1], '.pici/configs/' + thisapp.name + '.json')
             else:
                 print("Error loading " + cmd_args[1])
     if cmd_args[0] == "apps":
@@ -60,11 +86,14 @@ while True:
                     a.stop()
 
     if cmd_args[0] == "build":
-        if len(cmd_args) == 2:
+        if len(cmd_args) >= 2:
             # Get app
             for a in apps:
                 if a.name == cmd_args[1]:
-                    a.build()
+                    if len(cmd_args) == 3 and cmd_args[2] == "-s":
+                        a.build(False)
+                    else:
+                        a.build()
 
     if cmd_args[0] == "tail":
         if len(cmd_args) == 2:
@@ -72,6 +101,21 @@ while True:
             for a in apps:
                 if a.name == cmd_args[1]:
                     a.tail()
+
+    if cmd_args[0] == "output":
+        if len(cmd_args) == 2:
+            # Get app
+            if os.path.isfile('.pici/outputs/' + cmd_args[1] + '.out.log'):
+                with open('.pici/outputs/' + cmd_args[1] + '.out.log', 'r') as myfile:
+                    print(myfile.read())
+
+    if cmd_args[0] == "error":
+        if len(cmd_args) == 2:
+            # Get app
+            if os.path.isfile('.pici/outputs/' + cmd_args[1] + '.err.log'):
+                with open('.pici/outputs/' + cmd_args[1] + '.err.log', 'r') as myfile:
+                    print(myfile.read())
+                    
 
     if cmd_args[0] == "exit":
         break
--- pici/app.py
+++ pici/app.py
@@ -53,7 +53,7 @@ class App:
         appstderr = self.stderr if not outputHere else sys.stderr
 
         if not os.path.exists('.pici/apps/' + self.name):
-            print('PICI: Building app')
+            print('PiCI: Building app')
             os.makedirs('.pici/apps/' + self.name, exist_ok=True)
             self.buildproc = subprocess.Popen(['/usr/bin/git', 'clone', self.git, '.pici/apps/' + self.name], stdout=appstdout, stderr=appstderr)
             self.buildproc.wait()
@@ -64,35 +64,38 @@ class App:
             with open('.picigit', 'r') as f:
                 old_git = f.read()
             if old_git != self.git:
-                print('PICI: Rebuilding app')
+                print('PiCI: Rebuilding app')
                 shutil.rmtree('.pici/apps/' + self.name)
                 self.buildproc = subprocess.Popen(['/usr/bin/git', 'clone', self.git, '.pici/apps/' + self.name], stdout=appstdout, stderr=appstderr)
                 self.buildproc.wait()
                 with open('.picigit', 'w') as f:
                     print(self.git, file=f)
             else:
-                print('PICI: Pulling changes')
+                print('PiCI: Pulling changes')
                 self.buildproc = subprocess.Popen(['/usr/bin/git', 'pull'], cwd='.pici/apps/' + self.name, stdout=appstdout, stderr=appstderr)
                 self.buildproc.wait()
                 
-        print("PICI: Running build command")
+        print("PiCI: Running build command")
         self.buildproc = subprocess.Popen(shlex.split(self.build_command), cwd='.pici/apps/' + self.name, stdout=appstdout, stderr=appstderr)
         self.buildproc.wait()
         self.generate_nginx_conf()
         self.close_outputs()
-        print("PICI: Done building app")
+        print("PiCI: Done building app")
 
     def start(self):
-        print("PICI: Starting app")
+        print("PiCI: Starting app")
         self.setup_outputs()
         self.startproc = subprocess.Popen(shlex.split(self.start_command), cwd='.pici/apps/' + self.name, stdout=self.stdout, stderr=self.stderr)
+        print("PiCI: Started app")
 
     def stop(self):
-        self.startproc.kill()
+        print("PiCI: Stopping app")
+        self.startproc.terminate()
         self.close_outputs()
+        print("PiCI: Stopped app")
 
     def tail(self):
-        tailproc = subprocess.Popen(['/usr/bin/tail', '-f', '.pici/outputs/' + self.name + '.out.log'])
+        tailproc = subprocess.Popen(['/usr/bin/tail', '-f', '.pici/outputs/' + self.name + '.out.log'], stdout=sys.stdout, stderr=sys.stderr, stdin=sys.stdin)
         tailproc.wait()
 
     def setup_outputs(self):
@@ -107,6 +110,8 @@ class App:
             self.stdout.close()
         if self.stderr != None:
             self.stderr.close()
+        self.stdout = None
+        self.stderr = None
 
     def generate_nginx_conf(self):
         with open('.pici/apps/' + self.name + '/nginx.conf', 'w') as f:
